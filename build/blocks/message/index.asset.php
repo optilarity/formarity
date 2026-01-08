21eeab40bc3ce97ff70d2a725bf7eef70beb25a9
Puleeno Nguyen <puleeno@gmail.com>
2026-01-08 17:02:26 +0700

    fix form submit when click ok close popup message

--- build/blocks/message/index.asset.php
+++ build/blocks/message/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'ee9f2b99433c350bad18');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '893622c70799c78f04eb');
--- src/blocks/message/save.js
+++ src/blocks/message/save.js
@@ -1,25 +1,25 @@
 import { InnerBlocks, useBlockProps, RichText } from '@wordpress/block-editor';
 
-export default function save( { attributes } ) {
-	const blockProps = useBlockProps.save( {
+export default function save({ attributes }) {
+	const blockProps = useBlockProps.save({
 		className: `wp-block-formello-message ${attributes.messageType || 'success'} ${attributes.displayType || 'notice'}`,
-	} );
+	});
 
-	if ( attributes.displayType === 'modal' ) {
+	if (attributes.displayType === 'modal') {
 		return (
 			<div
-				{ ...blockProps }
+				{...blockProps}
 				data-wp-interactive="formello"
-				data-wp-context={ JSON.stringify( {
+				data-wp-context={JSON.stringify({
 					successMessage: attributes.text,
 					messageType: attributes.messageType,
 					displayType: attributes.displayType,
-				} ) }
+				})}
 				data-wp-bind--hidden="!context.response"
 			>
 				<div className="modal-overlay" data-wp-bind--hidden="!context.response">
 					<div className="modal-content">
-						<button className="modal-close" data-wp-on--click="actions.closeModal">×</button>
+						<button type="button" className="modal-close" data-wp-on--click="actions.closeModal">×</button>
 						<div className="modal-icon">
 							{attributes.messageType === 'success' && '✓'}
 							{attributes.messageType === 'error' && '✕'}
@@ -28,7 +28,7 @@ export default function save( { attributes } ) {
 						</div>
 						<RichText.Content
 							tagName="p"
-							value={ attributes.text }
+							value={attributes.text}
 							data-wp-text="state.message"
 						/>
 						<ul data-wp-context="state.errors" data-wp-bind--hidden="context.response.success">
@@ -36,9 +36,9 @@ export default function save( { attributes } ) {
 								<li data-wp-text="context.item"></li>
 							</template>
 						</ul>
-					<button className="modal-ok" data-wp-on--click="actions.closeModal">
-						OK
-					</button>
+						<button type="button" className="modal-ok" data-wp-on--click="actions.closeModal">
+							OK
+						</button>
 					</div>
 				</div>
 				<InnerBlocks.Content />
@@ -48,19 +48,19 @@ export default function save( { attributes } ) {
 
 	return (
 		<div
-			{ ...blockProps }
+			{...blockProps}
 			data-wp-class--success="context.response.success"
 			data-wp-class--error="!context.response.success"
 			data-wp-interactive="formello"
-			data-wp-context={ JSON.stringify( {
+			data-wp-context={JSON.stringify({
 				successMessage: attributes.text,
 				messageType: attributes.messageType,
 				displayType: attributes.displayType,
-			} ) }
+			})}
 		>
 			<RichText.Content
 				tagName="p"
-				value={ attributes.text }
+				value={attributes.text}
 				data-wp-text="state.message"
 			/>
 			<ul data-wp-context="state.errors">
